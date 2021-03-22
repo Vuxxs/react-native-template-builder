3045e063935a3165d7b252100e8b19e4dde1d5bf
Vuxxs <vamvakaspanayiotis@gmail.com>
2021-03-22 11:06:51 +0200

    App's structure

---
+++ printTest.py
@@ -0,0 +1,4 @@
+# Εκτύπωστε τα components εδώ για να δείτε αν βγαίνουν σωστά
+from source.components import getText
+
+print(getText())
---
+++ source/components.py
@@ -0,0 +1,4 @@
+# Μια γραμμή για αυτά, τα opacity θα έχουν δικό τους section
+
+def getText():
+    return "<Text style={styles.titleText} onPress={onPressTitle}> </Text>"
