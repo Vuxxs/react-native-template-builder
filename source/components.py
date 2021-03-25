dc7c3d0edbd1cb53ad7dd81219061389ec4ac4cd
Vuxxs <vamvakaspanayiotis@gmail.com>
2021-03-25 13:05:06 +0200

    components fix & dummy template
    έβγαλα ότι δε χρειαζόμαστε και έφτιαξα το spacing στα components ανάλογα με το τι βγαίνει καλύτερα στο Dummy αρχείο που τα έχει όλα για testing

--- source/components.py
+++ source/components.py
@@ -1,103 +1,48 @@
 # Μια γραμμή για αυτά πάνω κάτω, τα Touchable θα έχουν δικό τους section.
 # Να μη ξεχάσουμε να βάλλουμε dummy values σε όσα χρειάζονται
 
-def getActivityIndicator():
-    return "<ActivityIndicator />"
-
-
 def getButton():
     return """<Button
-  onPress={() => console.log("Button pressed!")}
-  title="Learn More"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>"""
-
-
-def getFlatList():
-    return """<FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />"""
+                    title="Press me"
+                    color="#f194ff"
+                    onPress={() => Alert.alert("Button pressed!")}
+                />"""
 
 
 def getImage():
     return """<Image
-        style={styles.image}
-        source={require('@expo/snack-static/react-native-logo.png')}
-      />"""
-
-
-def getImageBackground():
-    return """<ImageBackground source={image} style={styles.image}>
-      <Text style={styles.text}>Inside</Text>
-    </ImageBackground>"""
+                    style={styles.image}
+                    source={this.image}
+                />"""
 
 
 def getPressable():
-    return """<Pressable onPress={onPressFunction}>
-  <Text>I'm pressable!</Text>
-</Pressable>"""
-
-
-def getRefreshControl():
-    return """<RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />"""
-
-
-def getSectionList():
-    return """<SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />"""
-
-
-def getStatusBar():
-    return """<StatusBar
-        animated={true}
-        backgroundColor="#61dafb"
-        barStyle={statusBarStyle}
-        showHideTransition={statusBarTransition}
-        hidden={hidden} />"""
+    return """<Pressable onPress={() => {
+                    Alert.alert("Pressable pressed!");
+                }}>
+                    <Text> I'm pressable!</Text>
+                </Pressable>"""
 
 
 def getSwitch():
     return """<Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />"""
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={() => { Alert.alert("Switch pressed!") }}
+                    value={isEnabled}
+                />"""
 
 
 def getText():
-    return "<Text style={styles.text}> </Text>"
+    return "<Text style={styles.text}> Plain text </Text>"
 
 
 def getTextInput():
     return """<TextInput
-        style={styles.textInput}
-        onChangeText={() => {console.log("text input changed")}}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />"""
-
-
-def getVirtualizedList():
-    return """<VirtualizedList
-        data={DATA}
-        initialNumToRender={4}
-        renderItem={({ item }) => <Item title={item.title} />}
-        keyExtractor={item => item.key}
-        getItemCount={getItemCount}
-        getItem={getItem}
-      />"""
+                    style={styles.textInput}
+                    onChangeText={() => { Alert.alert("TextInput changed!") }}
+                    value={10}
+                    placeholder="useless placeholder"
+                    keyboardType="numeric"
+                />"""
