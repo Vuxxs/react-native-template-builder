895824a7153ea7f03832da326dc91d7368c09d24
Vuxxs <vamvakaspanayiotis@gmail.com>
2021-03-25 14:36:14 +0200

    Μικρές αλλαγές για να δουλεύει με το GUI

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from time import sleep
 from tkinter import *
 from tkinter.ttk import Progressbar
+from source.builder import buildTemplate
 
-def panos():
+
+def execute():
+    # Fake progress bar lmao
     progress['value'] = 50
     window.update_idletasks()
     sleep(2)
-    # Εδώ ο Πάνος κάνει τα μαγικά του
-    print('panos')
+    buildTemplate()
     progress['value'] = 100
 
 
@@ -16,7 +18,7 @@ window.wm_geometry("300x200")
 
 Label(window, text="Δημιουργία React Native GUI!").pack()
 
-Button(window, text="Δημιουργία GUI", command=panos).pack()
+Button(window, text="Δημιουργία GUI", command=execute).pack()
 
 progress = Progressbar(window, orient=HORIZONTAL, length=200)
 progress.pack()
--- source/builder.py
+++ source/builder.py
@@ -2,62 +2,65 @@
 # Every phase is separated and made easy to edit if you are trying to generate templates based on your preference.
 
 from random import randrange
-from components import getButton, getImage, getPressable, getSwitch, getText, getTextInput
+from source.components import getButton, getImage, getPressable, getSwitch, getText, getTextInput
 
-# Initialization phase
-template = """import React, { Component } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
-import { Button, Image, Pressable, Switch, Text, TextInput } from 'react-native';\n"""
 
-# Generate default class
-template += """export default class Template extends Component {
+def buildTemplate():
+    # Initialization phase
+    template = """import React, { Component } from 'react';
+    import { View, StyleSheet, Alert } from 'react-native';
+    import { Button, Image, Pressable, Switch, Text, TextInput } from 'react-native';\n"""
 
-    image = require('./../dump/sample.jpg');
-    render() {
-        return (\n"""
+    # Generate default class
+    template += """export default class Template extends Component {
 
-# Generate View, the reason this isn't grouped up with default class is so you can change the View type if you choose to.
-template += "<View style={styles.container}>\n"
+        image = require('./../dump/sample.jpg');
+        render() {
+            return (\n"""
 
-# Randomization phase
+    # Generate View, the reason this isn't grouped up with default class is so you can change the View type if you choose to.
+    template += "<View style={styles.container}>\n"
 
-# Pick a random template and draw from './../templates/...'
+    # Randomization phase
 
-templates = ["Import files as an array from ./../templates/UI"]
-stylesheets = ["Import files as an array from ./../templates/SS"]
+    # Pick a random template and draw from './../templates/...'
 
-# Pick one of each randomly
-templatePicked = templates[randrange(len(templates))]
-stylesheetPicked = stylesheets[randrange(len(stylesheets))]
+    templates = ["Import files as an array from ./../templates/UI"]
+    stylesheets = ["Import files as an array from ./../templates/SS"]
 
-# Replace component code (#0000) with random component
-components = ["button", "image", "pressable", "switch", "text", "textInput"]
-while("0000" in templatePicked):
-    position = templatePicked.index("0000")
-    componentPicked = components[randrange(len(components))]
-    templatePicked = templatePicked[:position] + \
-        componentPicked + templatePicked[position+1:]
+    # Pick one of each randomly
+    templatePicked = templates[randrange(len(templates))]
+    stylesheetPicked = stylesheets[randrange(len(stylesheets))]
 
-# Place components to the positions defined
-
-for component in components:
-    while(component in templatePicked):
-        print("Found " + component + "! Replacing it appropriately..")
-        position = templatePicked.index(component)
+    # Replace component code (#0000) with random component
+    components = ["button", "image", "pressable",
+                  "switch", "text", "textInput"]
+    while("0000" in templatePicked):
+        position = templatePicked.index("0000")
+        componentPicked = components[randrange(len(components))]
         templatePicked = templatePicked[:position] + \
-            component + templatePicked[position+1:]
+            componentPicked + templatePicked[position+1:]
 
-# Append the ready-to-go template
-template += templatePicked + "\n"
-template += """</View>
-        );
-    }
-}\n"""
+    # Place components to the positions defined
 
-# Append the stylesheet and finish the process
-template += stylesheetPicked
+    for component in components:
+        while(component in templatePicked):
+            print("Found " + component + "! Replacing it appropriately..")
+            position = templatePicked.index(component)
+            templatePicked = templatePicked[:position] + \
+                component + templatePicked[position+1:]
 
-# Write to file
-f = open("Template.js", "w")
-f.write(template)
-f.close()
+    # Append the ready-to-go template
+    template += templatePicked + "\n"
+    template += """</View>
+            );
+        }
+    }\n"""
+
+    # Append the stylesheet and finish the process
+    template += stylesheetPicked
+
+    # Write to file
+    f = open("Template.js", "w")
+    f.write(template)
+    f.close()
