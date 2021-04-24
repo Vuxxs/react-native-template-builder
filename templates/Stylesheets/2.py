6bdfffa795f84699788120be5f8c93acd32de7c6
ΓΕΩΡΓΙΟΣ ΒΡΟΥΖΟΣ Π2019026 <72497431+GVR8@users.noreply.github.com>
2021-04-24 16:19:22 +0300

    Add files via upload

---
+++ templates/Stylesheets/2.py
@@ -0,0 +1,24 @@
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: "#297867",
+    },
+
+   image: {
+        resizeMode: "contain",
+        height: 75,
+        width: 80
+    },
+
+    text: {
+        fontSize: 15,
+        fontWeight: "bold"
+    },
+
+    textInput: {
+        height: 50,
+        margin: 50,
+        borderWidth: 5
+    }
+});
